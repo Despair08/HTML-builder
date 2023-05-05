ed755d62a42dd4c17be0452aedd91c8b5391bc6c
Despair08 <vodanila2008@ya.ru>
2023-05-05 06:44:22 +0300

    feat: write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin,stdout,stderr } = process;
+const rl = require('node:readline');
+
+
+stdout.write('Ну ты и соня! Тебя даже вчерашний шторм не разбудил.. \n');
+
+fs.writeFile(
+    path.join(__dirname, 'userInput.txt'),
+    '',
+    (err) => {
+        if (err) throw err;
+    }
+);
+
+stdin.on('data',data => fs.appendFile(
+  path.join(__dirname, 'userInput.txt'),
+  data,
+  (err) =>{
+    if(err)throw err;
+    if(data.toString().trim() === 'exit'){
+      process.exit(0);
+    }
+  }
+))
+
+process.on('exit', code => {
+if(code === 0){
+  stdout.write('Удачи')
+}else{
+  stderr.write(`Что то пошло не так ${code}`)
+}
+})
+
+process.on('SIGINT', () => {
+  process.exit(0)
+})
