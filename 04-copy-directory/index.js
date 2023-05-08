6ff660011e68d28f3cc9e813807af9665b9848af
Despair08 <vodanila2008@ya.ru>
2023-05-08 14:21:47 +0300

    feat: actualize  copy-files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,13 +5,15 @@ const fsPromises = fs.promises;
 async function copyDir(){
   try {
     const destFolder = path.join(__dirname, 'copy_files');
+    const remove = await fsPromises.rm(destFolder,{recursive:true},(err)=>{
+      console.log(err)
+    })
     createDir = await fsPromises.mkdir(destFolder, {recursive:true});
     const files = await fsPromises.readdir(path.join(__dirname,'files'),{withFileTypes: true});
     for(const file of files){
-      if(!file.isFile()){
-        fsPromises.readdir(path.join(__dirname, 'files', file.name));
+      if(file.isFile()){
+        await fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname,'copy_files', file.name))
       }
-      await fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname,'copy_files', file.name))
     }
   } catch (error) {
     console.log(error);
