b0229221663b53003189e6b2fc47735cd2d396c8
Despair08 <vodanila2008@ya.ru>
2023-05-08 09:46:32 +0300

    feat: copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path  = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
+async function copyDir(){
+  try {
+    const destFolder = path.join(__dirname, 'copy_files');
+    createDir = await fsPromises.mkdir(destFolder, {recursive:true});
+    const files = await fsPromises.readdir(path.join(__dirname,'files'),{withFileTypes: true});
+    for(const file of files){
+      if(!file.isFile()){
+        fsPromises.readdir(path.join(__dirname, 'files', file.name));
+      }
+      await fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname,'copy_files', file.name))
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+copyDir();
